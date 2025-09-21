7878401752cf2223af932fcd068c1d296468b954
Jason Hoang <jasonhoang6201@gmail.com>
2025-09-22 02:20:26 +0700

    fix: header

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <nav
-	class="fixed top-0 right-0 left-0 z-50 transition-all duration-300"
+	class="fixed top-0 right-0 left-0 z-50 hidden transition-all duration-300 md:block"
 	class:glass-card-static={isScrolled}
 >
 	<div class="container mx-auto flex items-center justify-between px-6 py-4 md:px-12">
-		<a href="#home" class="text-glow text-xl font-bold">HN</a>
-		<div class="hidden items-center space-x-8 md:flex">
+		<a href="#home" class="text-glow text-xl font-bold">JH</a>
+		<div class="flex items-center space-x-8">
 			{#each navLinks as link}
 				<a href={`#${link.href}`} class="link-text transition-colors duration-300">{link.label}</a>
 			{/each}
